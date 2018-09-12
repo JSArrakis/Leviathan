2f6c931d161b0dc06d24eacbc6c8da96747c4385
JSArrakis <jsarrakis.creddit@gmail.com>
2018-09-11 19:50:59 -0700

    Testing socket.io functionality

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
-var five = require("johnny-five");
+// var five = require("johnny-five");
 
-// Johnny-Five will try its hardest to detect the port for you,
-// however you may also explicitly specify the port by passing
-// it as an optional property to the Board constructor:
+// // Johnny-Five will try its hardest to detect the port for you,
+// // however you may also explicitly specify the port by passing
+// // it as an optional property to the Board constructor:
+// var board = new five.Board({
+//   port: "/dev/ttyACM0"
+// });
+
+// // The board's pins will not be accessible until
+// // the board has reported that it is ready
+// board.on("ready", function() {
+//   this.pinMode(13, this.MODES.OUTPUT);
+
+//   this.loop(500, () => {
+//     // Whatever the last value was, write the opposite
+//     this.digitalWrite(13, this.pins[13].value ? 0 : 1);
+//   });
+// });
+
+var app = require("express")();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+var five = require("johnny-five");
 var board = new five.Board({
   port: "/dev/ttyACM0"
 });
 
-// The board's pins will not be accessible until
-// the board has reported that it is ready
 board.on("ready", function() {
-  this.pinMode(13, this.MODES.OUTPUT);
+  var led = new five.Led(13);
+  led.blink(1000);
 
-  this.loop(500, () => {
-    // Whatever the last value was, write the opposite
-    this.digitalWrite(13, this.pins[13].value ? 0 : 1);
+  io.on("connection", function(socket) {
+    socket.on("change:interval", function(data) {
+      led.blink(data);
+    });
   });
 });
+
+app.get("/", function (req, res) {
+  res.sendFile("/home/pi/Leviathan/realtime.html");
+});
+
+server.listen(3000);
+
