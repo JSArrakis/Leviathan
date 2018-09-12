cb042b4ae757d0b611a7670d8881910e9a001027
JSArrakis <jsarrakis.creddit@gmail.com>
2018-09-12 15:56:51 -0700

    Adding blinking light (not connected to socket yet)

--- public/js/app.js
+++ public/js/app.js
@@ -21,10 +21,10 @@
 var app = require("express")();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-// var five = require("johnny-five");
-// var board = new five.Board({
-//   port: "/dev/ttyACM0"
-// });
+var five = require("johnny-five");
+var board = new five.Board({
+  port: "/dev/ttyACM0"
+});
 
 console.log("Ready!");
 
@@ -34,12 +34,10 @@ io.on("connection", function(socket) {
     });
 });
 
-// board.on("ready", function() {
-//   var led = new five.Led(13);
-//   led.blink(1000);
-
-  
-// });
+board.on("ready", function() {
+  var led = new five.Led(13);
+  led.blink(1000);
+});
 
 app.get("/", function (req, res) {
   res.sendFile("/home/pi/Leviathan/public/index.html");
